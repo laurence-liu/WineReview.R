aea12da1ae209f2e05ecf9328af877680964a677
laurence-liu <laurence.liuuu@gmail.com>
2019-03-04 17:37:24 -0800

    Add data frame and some function(More in description)
    1. implement dim and summary
    2. import ggmap
    3. find top 5 rating province
    4. find max and min price
    5. add latitude and longitude to every country

--- WineReview.R
+++ WineReview.R
@@ -12,14 +12,40 @@ df.wine <- rbind(df.wine1[keeps.wine], df.wine2[keeps.wine])
 # Fliter NA vlaue
 df.wine <- na.omit(df.wine)
 
+# data frame dim andsummary
+dim(df.wine)
+summary(df.wine)
+
 # import library dplyr
 library(dplyr)
 # import library ggplot2
 library(ggplot2)
+# import library ggmap and set API key, 
+# Reference: https://www.littlemissdata.com/blog/maps
+# Reference: https://blog.dominodatalab.com/geographic-visualization-with-rs-ggmaps/
+library(ggmap)
 
-# 1. Get country table
+# Find top 5 rating countries
 table.country <- table(df.wine$country)
-df.country <- subset(data.frame(table.country), Freq > 1000)
+df.country <- data.frame(country=table.country)
+colnames(df.country) <- c("Country", "Freq")
 tail(arrange(df.country, Freq), 5)
 
-# Hi I am Sophie
+# Find top 5 rating province
+table.province <- table(df.wine$province)
+df.province <- subset(data.frame(table.province))
+colnames(df.province) <- c("Province", "Freq")
+tail(arrange(df.province, Freq), 5)
+
+# Find max and min price
+price.max <- df.wine[which.max(df.wine$price),]
+price.min <- df.wine[which.min(df.wine$price),]
+
+# Get every latitude and longitude of every country
+for (i in 1:nrow(df.country)) {
+  latlng = geocode(as.character(df.country$Country[i]))
+  df.country$latitude[i] = as.numeric(latlng)[2]
+  df.country$longitude[i] = as.numeric(latlng)[1]
+}
+
+
